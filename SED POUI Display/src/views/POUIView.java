e5da58cdea020d0cdffd7218aff080372629d95b
James Chapman <jjrchapman92@gmail.com>
2017-10-22 13:23:15 -0600

    Created basic view to display POUI image and previous/next buttons

--- SED POUI Display/src/views/POUIView.java
+++ SED POUI Display/src/views/POUIView.java
@@ -1,10 +1,81 @@
 package views;
 
+import java.awt.BorderLayout;
+import java.io.IOException;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import customtypes.POUI;
+
 /**
  * This view will display the POUI images as well as provide the basic interface for iterating through
  * the different steps.
  * @author jameschapman
  */
 public class POUIView {
+	private POUI assemblyPOUI;
+	private JFrame mainFrame;
 
+	public POUIView(POUI assemblyPOUI) {
+		this.assemblyPOUI = assemblyPOUI;
+		mainFrame = new JFrame();
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.add(createPanel());
+		mainFrame.pack();
+		mainFrame.setVisible(true);
+	}
+
+	private JPanel createPanel() {
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		
+		JPanel imagePane = new JPanel();
+		imagePane.setLayout(new BoxLayout(imagePane, BoxLayout.PAGE_AXIS));
+		imagePane.setBorder(BorderFactory.createEmptyBorder(15,15,15,15));
+		
+		// add image to panel
+		JLabel poui = new JLabel(new ImageIcon(assemblyPOUI.nextStep()));
+		poui.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		imagePane.add(poui);
+		
+		// create pane for buttons
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.LINE_AXIS));
+		
+		// create and add previous button to panel
+		JButton previous = new JButton("Previous");
+		previous.setAlignmentX(JButton.LEFT_ALIGNMENT);
+		buttonPane.add(previous);
+		
+		buttonPane.add(Box.createHorizontalGlue());
+		
+		// create and add next button to panel
+		JButton next = new JButton("Next");
+		next.setAlignmentX(JButton.RIGHT_ALIGNMENT);
+		buttonPane.add(next);
+		
+		// add image and buttons to mainPanel
+		mainPanel.add(imagePane, BorderLayout.CENTER);
+		mainPanel.add(buttonPane, BorderLayout.PAGE_END);
+		
+		return mainPanel;
+	}
+	
+	public static void main(String[] args) {
+		POUI test;
+		try {
+			test = new POUI(3, "/Users/jameschapman/Projects/SED Projects/poui-displayer/Sample Images/");
+			POUIView testView = new POUIView(test);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
