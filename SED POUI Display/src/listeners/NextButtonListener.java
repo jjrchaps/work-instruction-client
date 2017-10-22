5d6b844989c0887e1cc89ddc970022c47043182b
James Chapman <jjrchapman92@gmail.com>
2017-10-22 14:01:04 -0600

    Created and implemented ActionListener for the next button

---
+++ SED POUI Display/src/listeners/NextButtonListener.java
@@ -0,0 +1,39 @@
+package listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import customtypes.POUI;
+
+/**
+ * Listener for the next button in the pouiview class. If there is another image, it will display it.
+ * @author jameschapman
+ */
+public class NextButtonListener implements ActionListener {
+	// save an instance of the POUI we're working on to pull up future steps
+	private POUI poui;
+	// instance of the imagePanel that is displayed in the POUIView
+	private JLabel imageLabel;
+	
+	/**
+	 * Prepares local instances of variables to be able to make the image swap
+	 * @param poui The source POUI object to get the images from
+	 * @param imagePanel The pane that the images are being displayed in
+	 */
+	public NextButtonListener(POUI poui, JLabel imageLabel) {
+		this.poui = poui;
+		this.imageLabel = imageLabel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (poui.hasNext()) {
+			imageLabel.setIcon(new ImageIcon(poui.nextStep()));
+		}
+	}
+
+}
