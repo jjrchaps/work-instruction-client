e7c4f147c1bb91ffc7fb8971092b2016ddcac840
James Chapman <jjrchapman92@gmail.com>
2017-10-22 10:48:57 -0600

    Added POUIView class

---
+++ SED POUI Display/src/views/POUIView.java
@@ -0,0 +1,10 @@
+package views;
+
+/**
+ * This view will display the POUI images as well as provide the basic interface for iterating through
+ * the different steps.
+ * @author jameschapman
+ */
+public class POUIView {
+
+}
