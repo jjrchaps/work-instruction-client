610e40d182741923551e663e6dd7efa8ce30f328
James Chapman <jjrchapman92@gmail.com>
2017-10-22 14:44:17 -0600

    Removed superfluous code from the main entry point of the system

--- SED POUI Display/src/main/AssemblyPOUIDisplayer.java
+++ SED POUI Display/src/main/AssemblyPOUIDisplayer.java
@@ -1,16 +1,9 @@
 package main;
 
-import java.util.ArrayList;
-
-import customtypes.POUI;
 import views.StartupView;
 
 public class AssemblyPOUIDisplayer {
-	public static ArrayList<POUI> pouiContainer;
-	
 	public static void main(String[] args) {
-		pouiContainer = new ArrayList<POUI>();
-		// add POUI to the container. this is temporary until server is created.
 		StartupView startup = new StartupView();
 		startup.makeVisible();
 	}
