cbd378d78dd351ad5dfa1d6cc4b8e143b901956b
James Chapman <jjrchapman92@gmail.com>
2017-10-22 10:04:39 -0600

    Add POUI class in customtypes package to store assembly instructions

---
+++ SED POUI Display/src/customtypes/POUI.java
@@ -0,0 +1,12 @@
+package customtypes;
+
+/**
+ * POUI will contain the images that will be used for assembler instruction, as well as relevant meta data 
+ * about that particular assembly.
+ * @author jameschapman
+ */
+public class POUI {
+	// TODO: Needs a way to store the images within the object
+	// TODO: number of steps required to build
+	// TODO: methods to return next step, go back a step, etc.
+}
