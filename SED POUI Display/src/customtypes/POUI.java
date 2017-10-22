d62f3153d5a704c73b7869e79a52f962c31676d4
James Chapman <jjrchapman92@gmail.com>
2017-10-22 10:13:18 -0600

    Added comments for future development guidance

--- SED POUI Display/src/customtypes/POUI.java
+++ SED POUI Display/src/customtypes/POUI.java
@@ -6,7 +6,9 @@ package customtypes;
  * @author jameschapman
  */
 public class POUI {
+	// TODO: Add constructor that will read number of steps and folder in which images are located, and read the images in order.
 	// TODO: Needs a way to store the images within the object
 	// TODO: number of steps required to build
 	// TODO: methods to return next step, go back a step, etc.
+	// TODO: equals method that will simply check if the SED ID number is the same, we don't need to check more than that.
 }
