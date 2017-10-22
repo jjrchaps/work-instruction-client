086d114b5a58e1d20c8587c99a917c61d368af63
James Chapman <jjrchapman92@gmail.com>
2017-10-22 14:13:49 -0600

    Removed unnecessary imports

--- SED POUI Display/src/customtypes/POUI.java
+++ SED POUI Display/src/customtypes/POUI.java
@@ -3,8 +3,6 @@ package customtypes;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
