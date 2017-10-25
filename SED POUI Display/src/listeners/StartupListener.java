a731c77d19bd19daf1da83577ccb1d5d1b50acc4
James Chapman <jjrchapman92@gmail.com>
2017-10-24 18:41:45 -0600

    Began implementation of fetching images from server

--- SED POUI Display/src/listeners/StartupListener.java
+++ SED POUI Display/src/listeners/StartupListener.java
@@ -4,8 +4,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -72,5 +78,36 @@ public class StartupListener implements ActionListener, KeyListener {
 			}
 		}
 	}
+
+	//	@SuppressWarnings("unchecked")
+	//	private void getPOUI() {
+	//		Socket clientSocket;
+	//		try {
+	//			clientSocket = new Socket("localhost", 12312);
+	//			PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+	//			ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+	//			System.out.println("Connected");
+	//			out.println(textfield.getText());
+	//			out.flush();
+	//			LinkedList<ImageIcon> receivedImages = (LinkedList<ImageIcon>) in.readObject();
+	//			if (receivedImages != null) {
+	//				ClientPOUI poui = new ClientPOUI(receivedImages);
+	//				POUIView assemblyView = new POUIView(poui);
+	//				assemblyView.setVisible();
+	//			}
+	//			else {
+	//				textfield.setText("Please enter a valid product number");
+	//			}
+	//		} catch (UnknownHostException e) {
+	//			// TODO Auto-generated catch block
+	//			e.printStackTrace();
+	//		} catch (IOException e) {
+	//			// TODO Auto-generated catch block
+	//			e.printStackTrace();
+	//		} catch (ClassNotFoundException e) {
+	//			// TODO Auto-generated catch block
+	//			e.printStackTrace();
+	//		}
+	//	}
 
 }	
