ecc3d9105bc7ae85c7da3607d416d03f7f60f6ce
James Chapman <jjrchapman92@gmail.com>
2017-10-29 21:16:55 -0600

    Added logic to hide 'Build Complete' button dynamically

--- SED POUI Display/src/listeners/PreviousButtonListener.java
+++ SED POUI Display/src/listeners/PreviousButtonListener.java
@@ -3,6 +3,7 @@ package listeners;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import customTypes.ClientPOUI;
@@ -16,21 +17,28 @@ public class PreviousButtonListener implements ActionListener {
 	private ClientPOUI poui;
 	// instance of the imagePanel that is displayed in the POUIView
 	private JLabel imageLabel;
+	// instance of JButton to store the build complete button, this allows it to be dynamically
+	// controlled as the directions are followed
+	private JButton completeButton;
 	
 	/**
 	 * Prepares local instances of variables to be able to make the image swap
 	 * @param poui The source POUI object to get the images from
 	 * @param imagePanel The pane that the images are being displayed in
 	 */
-	public PreviousButtonListener(ClientPOUI poui, JLabel imageLabel) {
+	public PreviousButtonListener(ClientPOUI poui, JLabel imageLabel, JButton completeButton) {
 		this.poui = poui;
 		this.imageLabel = imageLabel;
+		this.completeButton = completeButton;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (poui.hasPrevious()) {
 			imageLabel.setIcon(poui.previousStep());
+			if (completeButton.isVisible()) {
+				completeButton.setVisible(false);
+			}
 		}
 	}
 
