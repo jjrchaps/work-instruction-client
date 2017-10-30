870e454e7fcd38de9b60e225f6314c8e624c5d72
James Chapman <jjrchapman92@gmail.com>
2017-10-29 21:24:26 -0600

    Added action listener to build complete button

--- SED POUI Display/src/views/POUIView.java
+++ SED POUI Display/src/views/POUIView.java
@@ -11,6 +11,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import customTypes.ClientPOUI;
+import listeners.BuildCompleteListener;
 import listeners.NextButtonListener;
 import listeners.PreviousButtonListener;
 
@@ -35,60 +36,62 @@ public class POUIView {
 	private JPanel createPanel() {
 		JPanel mainPanel = new JPanel();
 		mainPanel.setLayout(new BorderLayout());
-		
+
 		JPanel imagePane = new JPanel();
 		imagePane.setLayout(new BoxLayout(imagePane, BoxLayout.PAGE_AXIS));
 		imagePane.setBorder(BorderFactory.createEmptyBorder(15,15,15,15));
-		
+
 		// add image to panel
 		JLabel poui = new JLabel(assemblyPOUI.nextStep());
 		poui.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 		imagePane.add(poui);
-		
+
 		// create pane for buttons
 		buttonPane = new JPanel();
 		buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.LINE_AXIS));
-		
+
 		// create and add previous button to panel
 		JButton previous = new JButton("Previous");
 		previous.setAlignmentX(JButton.LEFT_ALIGNMENT);
-		
+
 		buttonPane.add(previous);
-		
+
 		buttonPane.add(Box.createHorizontalGlue());
-		
+
 		JButton complete = new JButton("Build Complete");
 		complete.setAlignmentX(JButton.CENTER_ALIGNMENT);
 		complete.setVisible(false);
 		buttonPane.add(complete);
 		buttonPane.add(Box.createHorizontalGlue());
-		
+
 		// create and add next button to panel
 		JButton next = new JButton("Next");
 		next.setAlignmentX(JButton.RIGHT_ALIGNMENT);
 		buttonPane.add(next);
-		
+
 		// add action listeners to buttons
 		PreviousButtonListener previousListener = new PreviousButtonListener(assemblyPOUI, poui, complete);
 		previous.addActionListener(previousListener);
 		NextButtonListener nextListener = new NextButtonListener(assemblyPOUI, poui, complete);
 		next.addActionListener(nextListener);
-		
+		BuildCompleteListener completeListener = new BuildCompleteListener(mainFrame);
+		complete.addActionListener(completeListener);
+
 		// add image and buttons to mainPanel
 		mainPanel.add(imagePane, BorderLayout.CENTER);
 		mainPanel.add(buttonPane, BorderLayout.PAGE_END);
-		
+
 		return mainPanel;
 	}
-	
+
 	public void setVisible() {
 		mainFrame.setVisible(true);
 	}
-	
+
 	public void hideView() {
 		mainFrame.setVisible(false);
 	}
-	
+
 	public void showCompleteButton() {
 	}
 }
