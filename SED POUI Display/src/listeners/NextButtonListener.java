27542b6a8ed8a2ca23b482c1826c7ffe4ab1093f
James Chapman <jjrchapman92@gmail.com>
2017-10-29 21:16:39 -0600

    Added logic to display 'Build Complete' button

--- SED POUI Display/src/listeners/NextButtonListener.java
+++ SED POUI Display/src/listeners/NextButtonListener.java
@@ -3,9 +3,11 @@ package listeners;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import customTypes.ClientPOUI;
+import views.POUIView;
 
 /**
  * Listener for the next button in the pouiview class. If there is another image, it will display it.
@@ -22,20 +24,26 @@ public class NextButtonListener implements ActionListener {
 	 */
 	private JLabel imageLabel;
 	
+	private JButton completeButton;
+	
 	/**
 	 * Prepares local instances of variables to be able to make the image swap
 	 * @param poui The source POUI object to get the images from
 	 * @param imagePanel The pane that the images are being displayed in
 	 */
-	public NextButtonListener(ClientPOUI poui, JLabel imageLabel) {
+	public NextButtonListener(ClientPOUI poui, JLabel imageLabel, JButton completeButton) {
 		this.poui = poui;
 		this.imageLabel = imageLabel;
+		this.completeButton = completeButton;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (poui.hasNext()) {
 			imageLabel.setIcon(poui.nextStep());
+			if (!(poui.hasNext())) {
+				completeButton.setVisible(true);
+			}
 		}
 	}
 
