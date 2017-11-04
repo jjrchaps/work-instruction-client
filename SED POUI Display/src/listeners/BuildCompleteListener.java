9606bb9732237cd7cc904ef6f00f9394d313d291
James Chapman <jjrchapman92@gmail.com>
2017-11-04 13:54:52 -0600

    Added logic to initiate the time reporting to the server on click event.

--- SED POUI Display/src/listeners/BuildCompleteListener.java
+++ SED POUI Display/src/listeners/BuildCompleteListener.java
@@ -5,6 +5,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 
+import connections.ServerConnection;
+import customTypes.ClientPOUI;
+
 /**
  * BuildCompleteListener is an action listener for the 'Build Complete' button in
  * POUI view frame. For now, it simply hides the frame when clicked. Future development
@@ -19,12 +22,32 @@ public class BuildCompleteListener implements ActionListener {
 	 */
 	private JFrame viewFrame;
 	
-	public BuildCompleteListener(JFrame viewFrame) {
+	/**
+	 * Connection to the POUI server that will be used to report build times.
+	 */
+	private ServerConnection connection;
+	
+	/**
+	 * The assembly that will be completed where the build timings can be fetched.
+	 */
+	private ClientPOUI poui;
+	
+	/**
+	 * Creates a new instance of the build complete button, and takes in as parameters
+	 * the frame it's within as well as a connection to the server to report build timings.
+	 * @param viewFrame The frame that this listener belongs to
+	 * @param connection A connection with the POUI server
+	 * @paran poui The poui that's currently being displayed to the user.
+	 */
+	public BuildCompleteListener(JFrame viewFrame, ServerConnection connection, ClientPOUI poui) {
 		this.viewFrame = viewFrame;
+		this.connection = connection;
+		this.poui = poui;
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		connection.reportTimings(poui.getTimings());
 		viewFrame.setVisible(false);
 	}
 
