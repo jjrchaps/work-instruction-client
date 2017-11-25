b4a053657ca078817987249b6f5c75a379c502eb
James Chapman <jjrchapman92@gmail.com>
2017-11-25 15:34:15 -0600

    Next button now checks if inspect necessary, triggers inspect view if so

--- SED POUI Display/src/listeners/NextButtonListener.java
+++ SED POUI Display/src/listeners/NextButtonListener.java
@@ -7,6 +7,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import auxiliary.ClientPOUI;
+import views.InspectionAuthenticationView;
 
 /**
  * Listener for the next button in the pouiview class. If there is another image, it will display it.
@@ -17,23 +18,22 @@ public class NextButtonListener implements ActionListener {
 	 * save an instance of the POUI we're working on to pull up future steps
 	 */
 	private ClientPOUI poui;
-	
+
 	/**
 	 * instance of the imagePanel that is displayed in the POUIView
 	 */
 	private JLabel imageLabel;
-	
+
 	/**
 	 * complete button instance so that it's visibility can be dynamically changed
 	 */
 	private JButton completeButton;
-	
+
 	/**
 	 * Prepares local instance variables for all parameters so the button is able to swap images when desired.
 	 * @param poui The poui that is currently being displayed
 	 * @param imageLabel The JLabel that is displaying the ImageIcon
 	 * @param completeButton A reference to the "Build Complete" button so we can dynamically display it
-	 * @param mainFrame The JFrame that the image is being displayed in, needed for resizing the frame to match the new image loaded.
 	 */
 	public NextButtonListener(ClientPOUI poui, JLabel imageLabel, JButton completeButton) {
 		this.poui = poui;
@@ -43,12 +43,22 @@ public class NextButtonListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (poui.hasNext()) {
-			imageLabel.setIcon(poui.nextStep());
-			if (!(poui.hasNext())) {
-				completeButton.setVisible(true);
-			}
+		if (poui.requiresInspection()) {
+			InspectionAuthenticationView inspectView = new InspectionAuthenticationView(this);
+			inspectView.setVisible();
+		}
+		else {
+			displayNext();
 		}
 	}
-
+	
+	public void displayNext() {
+		poui.setCurrentStepInspected();
+		if (poui.hasNext()) {
+				imageLabel.setIcon(poui.nextStep());
+				if (!(poui.hasNext())) {
+					completeButton.setVisible(true);
+				}
+			}
+	}
 }
