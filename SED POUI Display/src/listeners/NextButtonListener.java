157b6e1c231bb7fca76f60d72139e292f0baa042
James Chapman <jjrchapman92@gmail.com>
2017-11-26 18:07:18 -0600

    Minor adjustments, largest being setting authentication view to invisible once authenticated.

--- SED POUI Display/src/listeners/NextButtonListener.java
+++ SED POUI Display/src/listeners/NextButtonListener.java
@@ -7,6 +7,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import auxiliary.ClientPOUI;
+import connections.ClientServerConnection;
 import views.InspectionAuthenticationView;
 
 /**
@@ -28,37 +29,45 @@ public class NextButtonListener implements ActionListener {
 	 * complete button instance so that it's visibility can be dynamically changed
 	 */
 	private JButton completeButton;
-
+	
+	/**
+	 * A connection to the server, to be passed along to the inspection view when necessary
+	 */
+	private ClientServerConnection connection;
+	
 	/**
 	 * Prepares local instance variables for all parameters so the button is able to swap images when desired.
 	 * @param poui The poui that is currently being displayed
 	 * @param imageLabel The JLabel that is displaying the ImageIcon
 	 * @param completeButton A reference to the "Build Complete" button so we can dynamically display it
 	 */
-	public NextButtonListener(ClientPOUI poui, JLabel imageLabel, JButton completeButton) {
+	public NextButtonListener(ClientPOUI poui, JLabel imageLabel, JButton completeButton, ClientServerConnection connection) {
 		this.poui = poui;
 		this.imageLabel = imageLabel;
 		this.completeButton = completeButton;
+		this.connection = connection;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (poui.requiresInspection()) {
-			InspectionAuthenticationView inspectView = new InspectionAuthenticationView(this);
+			InspectionAuthenticationView inspectView = new InspectionAuthenticationView(this, connection, 
+					poui.getCurrentStepNumber(), poui.getProductID());
 			inspectView.setVisible();
 		}
 		else {
 			displayNext();
 		}
 	}
-	
+
 	public void displayNext() {
+		// if the user ID is null it means that no reporting to server is necessary.
 		poui.setCurrentStepInspected();
 		if (poui.hasNext()) {
-				imageLabel.setIcon(poui.nextStep());
-				if (!(poui.hasNext())) {
-					completeButton.setVisible(true);
-				}
+			imageLabel.setIcon(poui.nextStep());
+			if (!(poui.hasNext())) {
+				completeButton.setVisible(true);
 			}
+		}
 	}
 }
