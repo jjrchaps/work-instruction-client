501fb3e509b7f2ac7c57e277170eb75985bc71c0
James Chapman <jjrchapman92@gmail.com>
2017-11-28 21:08:33 -0600

    Removed sample images and added screenshots of systems current state

---
+++ SED POUI Display/src/actions/NextAction.java
@@ -0,0 +1,15 @@
+package actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+public class NextAction extends AbstractAction {
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+}
