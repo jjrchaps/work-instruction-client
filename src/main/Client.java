95341ccf6f3ed8ed6eb7cb40ea72f957c33d013b
James Chapman <jjrchapman92@gmail.com>
2018-04-15 13:03:54 -0600

    Edited startup procedure to open prompt for server ip and port

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,7 +1,6 @@
 package main;
 
-import connections.ClientServerConnection;
-import views.StartupView;
+import views.ConnectionDetailsPrompt;
 
 /**
  * This serves as the main entry point to the program. It establishes a connection with the POUI server
@@ -14,8 +13,7 @@ public class Client {
 	 * @param args Standard PSVM arguments, not currently used in this program.
 	 */
 	public static void main(String[] args) {
-		ClientServerConnection connection = new ClientServerConnection();
-		StartupView startup = new StartupView(connection);
-		startup.makeVisible();
+		ConnectionDetailsPrompt prompt = new ConnectionDetailsPrompt();
+		prompt.setVisible();
 	}
 }
